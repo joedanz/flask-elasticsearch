1a92b04d2062ffcc9b2a22fe32d9cb906de22db2
Joe Danziger <joe@ticc.net>
2015-05-05 00:03:23 -0400

    update booklist w/ local files

--- app/booklist_local.py
+++
@@ -1,5 +0,0 @@
-g.books = [
-    ["Pride and Prejudice","http://ticc.net/pg1342.txt"],
-    ["Alice's Adventures in Wonderland","http://ticc.net/pg11.txt"],
-    ["Adventures of Huckleberry Finn","http://ticc.net/pg76.txt"],
-]
